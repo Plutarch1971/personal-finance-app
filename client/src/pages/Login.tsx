8a3e787fb1d9faad0e6135721873497943873623
Matt-Verni <matthewpmendez@gmail.com>
2026-03-01 20:51:59 -0500

    Fixed Loging Page Card Layout Issue successfully

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -31,98 +31,91 @@ export default function Login() {
   };
 
   return (
-    <div className="container-fluid login-pg-background px-2 px-md-3 min-vh-100 py-4">
-      <div className="container fluid"> 
-              <div className="row mb-4 mt-2">
-                <div className="col-12 text-center">
-                <h1 className="text-white display-4 fw-bold">My Personal Finance</h1>
-                </div>
-              </div>
-                  {/* <div className="container"> */}
-                  <div className="row justify-content-center g-5 mx-0">
-                      <div className="col-12 col-md-6 col-xl-4 px-1 d-md-flex justify-content-center"> {/** 2/3 of 12 col */}
-                            <div className="card h-100 w-100" style={{ maxWidth: "500px", maxHeight: "500px"}}>
-                                <div className="card-body">
-                                    <p> Add text here</p>
-                                    <p> Add text here</p>
-                                    <p> Add text here</p>
-                                    <p> Add text here</p>
-                                    <p> Add text here</p>
-                                    <p> Add text here</p>
-                                    <p> Add text here</p>
-                                    <p> Add text here</p>
-                                </div> 
-                            </div>
+    <div className="container-fluid vh-100 d-flex flex-column login-pg-background">
+      <div className="text-center pt-5 mt-4">
+          <h1 className="text-white display-4 fw-bold">My Personal Finance</h1>          
+      </div>
+      <div className="flex-grow-1 d-flex justify-content-center align-items-center">
+            <div className="row w-100 justify-content-center g-4 align-items-stretch">
+                <div className="col-12 col-md-4 col-lg-3">
+                      <div className="card p-4 shadow" style={{ height: "420px"}}>
+                          <div className="card-body overflow-auto">
+                                <p> Add text here</p>
+                                <p> Add text here</p>
+                                <p> Add text here</p>
+                                <p> Add text here</p>
+                                <p> Add text here</p>
+                                <p> Add text here</p>
+                                <p> Add text here</p>
+                          </div>
                       </div>
-                      <div className="col-12 col-md-6 col-xl-4 px-1 d-flex flex-row">
-                        <div className="card h-100 w-100">
+                </div>
+                <div className="col-12 col-md-4 col-lg-3">
+                      <div className="card p-4 shadow" style={{ height: "420px"}}>
+                          <div className="card-body overflow-auto">
+                            <p> Add text here</p>
+                            <p> Add text here</p>
+                            <p> Add text here</p>
+                            <p> Add text here</p>
+                            <p> Add text here</p>
+                            <p> Add text here</p>
+                            <p> Add text here</p>
+                          </div>
+                      </div>
+                </div>
+                    <div className="col-12 col-md-4 col-lg-3">  
+                        <div className="card p-4 shadow" style={{ height: "420px"}}>
                           <div className="card-body">
-                            <p> Add text here</p>
-                            <p> Add text here</p>
-                            <p> Add text here</p>
-                            <p> Add text here</p>
-                            <p> Add text here</p>
-                            <p> Add text here</p>
-                            <p> Add text here</p>
-                            
-                      
+                            <h3 className="card-title text-center">Login</h3>
 
+                            {error && (
+                              <div className="alert alert-danger">{error}</div>
+                            )}
+
+                            <form onSubmit={handleSubmit}>
+                              <div className="mb-3">
+                                <label className="form-label">Email</label>
+                                <input
+                                  type="email"
+                                  className="form-control"
+                                  value={email}
+                                  required
+                                  onChange={(e) => setEmail(e.target.value)}
+                                />
+                              </div>
+
+                              <div className="mb-3">
+                                <label className="form-label">Password</label>
+                                <input
+                                  type="password"
+                                  className="form-control"
+                                  value={password}
+                                  required
+                                  onChange={(e) => setPassword(e.target.value)}
+                                />
+                              </div>
+
+                              <button
+                                type="submit"
+                                className="btn btn-primary w-100"
+                                disabled={loading}
+                              >
+                                {loading ? 'Logging in...' : 'Login'}
+                              </button>
+                            </form>
+                              <div className="text-center mt-3">
+                              <small>
+                                Don’t have an account?{' '}
+                                <Link to="/register">Register</Link>
+                              </small>
+                            </div>
                           </div>
                         </div>
                       </div>
-                          <div className="col-12 col-md-6 col-xl-4 px-1 d-flex justify-content-center align-self-center">  
-                              <div className="card shadow h-100 w-100">
-                                <div className="card-body bg-light">
-                                  <h3 className="card-title text-center mb-4">Login</h3>
-
-                                  {error && (
-                                    <div className="alert alert-danger">{error}</div>
-                                  )}
-
-                                  <form onSubmit={handleSubmit}>
-                                    <div className="mb-3">
-                                      <label className="form-label">Email</label>
-                                      <input
-                                        type="email"
-                                        className="form-control"
-                                        value={email}
-                                        required
-                                        onChange={(e) => setEmail(e.target.value)}
-                                      />
-                                    </div>
-
-                                    <div className="mb-3">
-                                      <label className="form-label">Password</label>
-                                      <input
-                                        type="password"
-                                        className="form-control"
-                                        value={password}
-                                        required
-                                        onChange={(e) => setPassword(e.target.value)}
-                                      />
-                                    </div>
-
-                                    <button
-                                      type="submit"
-                                      className="btn btn-primary w-100"
-                                      disabled={loading}
-                                    >
-                                      {loading ? 'Logging in...' : 'Login'}
-                                    </button>
-                                  </form>
-                                    <div className="text-center mt-3">
-                                    <small>
-                                      Don’t have an account?{' '}
-                                      <Link to="/register">Register</Link>
-                                    </small>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                        
+                  
                     </div>
-              {/* </div> */}
-         </div> 
+              </div>
+       
     </div>
 
   );
