4d88d2c26c72ffd9ace15d2073f85cab9aa7a7f5
Matt-Verni <matthewpmendez@gmail.com>
2026-05-18 17:56:27 -0400

    Configured Android Capacitor

---
+++ client/android/app/src/main/java/com/smartbooks/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.smartbooks.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,10 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: true,
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api', routes);
