e142e2b65553b58bf4116ebb712b22ffc179ab8f
Matt-Verni <matthewpmendez@gmail.com>
2026-05-19 21:17:06 -0400

    Fix app module export

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
+
+app.use(express.json());
+
+app.use('/api', routes);
+
+export default app;
