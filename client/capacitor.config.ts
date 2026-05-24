ca3d456ce14007dafcfdab3fca910fa7fcc88067
Matt-Verni <matthewpmendez@gmail.com>
2026-05-23 20:35:57 -0400

    Add and modified logos

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.smartbooks.app',
   appName: 'SmartBooks',
-  webDir: 'dist'
+  webDir: 'dist',
+
+  server: {
+    url: 'https://www.smartbooksfinance.com',
+    cleartext: false,
+    androidScheme: 'https'
+  }
 };
 
 export default config;
