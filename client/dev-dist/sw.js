3566fb36619b8da96edb0b8f56ceefd08e7d7a01
Matt-Verni <matthewpmendez@gmail.com>
2026-06-01 07:12:41 -0400

    Fix landing page mobile css settings

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u19jst4c6qg"
+    "revision": "0.p9d3jgc9lp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
