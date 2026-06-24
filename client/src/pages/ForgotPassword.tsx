aa4bfedbb91e6c8a9e337ac4cb0478d5fbc51b56
Matt-Verni <matthewpmendez@gmail.com>
2026-06-24 07:42:10 -0400

    Add result to send-reset-link button action in ForgotPassword.tsx

--- client/src/pages/ForgotPassword.tsx
+++ client/src/pages/ForgotPassword.tsx
@@ -4,19 +4,20 @@ import api from '../api/axios';
 
 const ForgotPassword: React.FC = () => {
     const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        // Call API to request password reset
+        setMessage('');
+        setError('');
+   
         try {
-            console.log("Submitting forgot password request");
-            const response = await api.post ('/auth/forgot-password',{ email });
-            console.log('Success:', response.data);
-        } catch (err: any) {
-            console.error('Status:', err.response?.status);
-            console.error('Data:', err.response?.data);
-            console.error('Error:', err);
-        }     
+                await api.post ('/auth/forgot-password',{ email });
+                setMessage('Password reset link sent! Check your inbox.');
+            } catch (err: any) {
+                setError('Something went wrong. Plese try again.');
+            }     
     };
 
     return (
@@ -25,6 +26,13 @@ const ForgotPassword: React.FC = () => {
             <div className="card shadow" style={{ width: '100%', maxWidth: '500px'}}>
                 <form className="mt-4 p-6" onSubmit={handleSubmit}>
                     <h4 className="text-center mb-4">Forgot Password</h4>
+
+                    {message && (
+                        <div className="alert alert-success">{message}</div>
+                    )}
+                    {error && (
+                        <div className="alert alert-danger">{error}</div>
+                    )}
                     <label className="fs-6 p-2">Enter email:</label>
                     <input
                         type="email"
