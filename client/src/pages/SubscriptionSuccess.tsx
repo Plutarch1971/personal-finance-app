537c2adfa367f35c466348953706a32d58908cf3
Matt-Verni <matthewpmendez@gmail.com>
2026-07-20 18:23:09 -0400

    Add dashboard button in Subscription success page

--- client/src/pages/SubscriptionSuccess.tsx
+++ client/src/pages/SubscriptionSuccess.tsx
@@ -1,9 +1,19 @@
 //SubscriptionSuccess.tsx
+import { useNavigate } from 'react-router-dom';
+
 export default function SubscriptionSuccess() {
+    const navigate = useNavigate();
+    
     return (
         <div className="text-white text-center d-flex align-items-center justify-content-center flex-column mt-4 p-4">
             <h1>Subscription Successful!</h1>
             <p>Your subscription has been activated.</p>
+            <button
+                className="btn btn-success"
+                onClick={() => navigate ("/dashboard")}
+            >
+                Dashboard
+            </button>
         </div>
     );
 }
