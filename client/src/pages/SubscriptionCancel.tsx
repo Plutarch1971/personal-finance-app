9fbd2b985eb607efeda04235351c788613bdb60f
Matt-Verni <matthewpmendez@gmail.com>
2026-07-20 18:14:53 -0400

    Add css to Subscription success and cancel pages

--- client/src/pages/SubscriptionCancel.tsx
+++ client/src/pages/SubscriptionCancel.tsx
@@ -1,7 +1,7 @@
 //SubscriptionCancel.tsx
 export default function SubscriptionCancel() {
     return (
-        <div>
+        <div className="text-white text-center d-flex align-items-center justify-content-center flex-column mt-4 p-4">
             <h1>Subscription Cancelled!</h1>
             <p>To continue using SmartBooksFinance, please Subscribe</p>
         </div>
--- client/src/pages/SubscriptionSuccess.tsx
+++ client/src/pages/SubscriptionSuccess.tsx
@@ -1,7 +1,7 @@
 //SubscriptionSuccess.tsx
 export default function SubscriptionSuccess() {
     return (
-        <div>
+        <div className="text-white text-center d-flex align-items-center justify-content-center flex-column mt-4 p-4">
             <h1>Subscription Successful!</h1>
             <p>Your subscription has been activated.</p>
         </div>
