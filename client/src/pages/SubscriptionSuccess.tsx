81e62428fd159827ba9e48d8e5b32c313e51e4cc
Matt-Verni <matthewpmendez@gmail.com>
2026-07-20 22:05:03 -0400

    Add auto redirection to dashboard from subscription success page

--- client/src/pages/SubscriptionSuccess.tsx
+++ client/src/pages/SubscriptionSuccess.tsx
@@ -1,19 +1,29 @@
 //SubscriptionSuccess.tsx
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function SubscriptionSuccess() {
     const navigate = useNavigate();
-    
+
+    useEffect(() => {
+        const timer = setTimeout(()=> {
+            navigate('/dashboard');
+        }, 2500);
+
+        return ()=> clearTimeout(timer);
+    }, [navigate]);
     return (
-        <div className="text-white text-center d-flex align-items-center justify-content-center flex-column mt-4 p-4">
-            <h1>Subscription Successful!</h1>
-            <p>Your subscription has been activated.</p>
-            <button
-                className="btn btn-success"
-                onClick={() => navigate ("/dashboard")}
-            >
-                Dashboard
-            </button>
+        <div className="container">
+            <div className="text-white text-center d-flex align-items-center justify-content-center flex-column mt-4 p-4">
+                <h1>Subscription Successful!</h1>
+                <p>Your subscription has been activated.</p>
+                    <button
+                    className="btn btn-success"
+                    onClick={() => navigate ("/dashboard")}
+                    >
+                       Go to Dashboard
+                    </button>
+            </div>
         </div>
     );
 }
